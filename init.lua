737c259a672eeac1b98b885c83082063f497d0d2
leo-moooney <118218097+leo-moooney@users.noreply.github.com>
2023-01-15 15:27:04 +1300

    Initial commit

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("leo.plugins-setup")
+require("leo.core.options")
+require("leo.core.keymaps")
+require("leo.core.colorscheme")
+require("leo.plugins.comment")
+require("leo.plugins.nvim-tree")
+require("leo.plugins.lualine")
+require("leo.plugins.telescope")
+require("leo.plugins.nvim-cmp")
+
