e18967093210dc70fffed6a36f7943fe39e3274f
Leo Mooney <leo.mooney@partly.com>
2026-02-11 15:26:21 +1300

    harpoon

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,3 +1,11 @@
 return {
-  "Pocco81/auto-save.nvim"
+  "Pocco81/auto-save.nvim",
+  opts = {
+    condition = function(buf)
+      if vim.bo[buf].filetype == "harpoon" then
+        return false
+      end
+      return true
+    end,
+  },
 }
